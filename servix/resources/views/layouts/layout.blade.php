8f12fe34e715b7f9500b1817b3390b452f785b12
aditya-shekhar773 <adityashekhar.773@gmail.com>
2023-03-21 17:50:56 +0530

    a

--- servix/resources/views/layouts/layout.blade.php
+++ servix/resources/views/layouts/layout.blade.php
@@ -28,7 +28,7 @@
 <div class="">
     <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">Login <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
     <!-- Dropdown menu -->
-    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown">
+    {{-- <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown">
         <div class="px-4 py-3">
         </div>
         <ul class="py-1" aria-labelledby="dropdown">
@@ -42,7 +42,7 @@
             <a href="" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
         </li>
         </ul>
-    </div>
+    </div> --}}
 </div>
       <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script> 
       </div>
