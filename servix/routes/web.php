d785a1c7c4b200d9a39d7f65fcf4f5aafc8ea6b1
sadique-cws <sadique.hussain96@gmail.com>
2023-03-22 17:39:45 +0530

    my commit

--- servix/routes/web.php
+++ servix/routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\StaffController;
 use App\Http\Controllers\AdminController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home');
     Route::get('/login', 'login')->name('login');
