fd92002c4852532ebaaf674591b0b90d892ba7f2
LazyDeveloperr <chhotaslaman07@gmail.com>
2023-03-23 01:19:55 +0530

    Updated staff section

--- servix/resources/views/staff/staffLogin.blade.php
+++ servix/resources/views/staff/staffLogin.blade.php
@@ -1,61 +1,133 @@
-@extends('layouts.layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
 
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{  asset("plugins/fontawesome-free/css/all.min.css") }}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{  asset("plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{  asset("plugins/icheck-bootstrap/icheck-bootstrap.min.css") }}">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{  asset("plugins/jqvmap/jqvmap.min.css") }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{  asset("dist/css/adminlte.min.css") }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{  asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{  asset("plugins/daterangepicker/daterangepicker.css") }}">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{  asset("plugins/summernote/summernote-bs4.min.css") }}">
+</head>
+<body class=" hold-transition login-page">
 
-<!-- Admin Login Form -->
-@section('contents')
-<div class="h1 pb-10 flex bg-gray-900 min-h-screen justify-center items-center mt-12">
-    <div class="container justify-around ">
-        <div class="w-full justify-around items-center  flex">
-            <div class="card bg-black w-2/6 rounded overflow-hidden mt-7">
-                <div class="min-w-screen bg-gray-900 flex items-center justify-center px-5 py-5">
-                    <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style="max-width:1000px">
-                        <div class="md:flex w-full">
-                            <div class="w-full py-10 px-5 md:px-10">
-                                <div class="text-center mb-10">
-                                    <h1 class="font-bold text-3xl text-gray-900">Staff Login</h1>
-                                </div>
-                            <div>
-                            <form action="" method="post">
-                                @csrf
-                                <div class="flex -mx-3">
-                                    <div class="w-full px-3 mb-5">
-                                        <label for="" class="text-xs font-semibold px-1">Email</label>
-                                        <div class="flex">
-                                            <input type="email" name="email" class="w-full  pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="example@gmail.com">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="flex -mx-3">
-                                    <div class="w-full px-3 mb-12">
-                                        <label for="" class="text-xs font-semibold px-1">Password</label>
-                                        <div class="flex">
-                                            <input type="password" name="password" class="w-full  pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************">
-                                        </div>
-                                    </div>
-                                </div>
-                                  <div class="flex -mx-3">
-                                    <div class="w-full px-3 mb-5">
-                                        <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">LOGIN</button>
-                                    </div>
-                                  </div>
-                            </form>
-                              <div class="flex -mx-3">
-                              <div class="w-full px-3 mb-5">
-                                    <div class="text-center items-center flex justify-center">
-                                        <span class="text-xs">I'm new here.</span>
-                                        <span class="font-semibold text-blue-600 mx-2"><a href="{{route('register')}}">Register Now</a></span>
-                                    </div>
-                                </div>
-                            </div>
-                         </div>
-                     </div>
-                  </div>
-                </div>
-            </div>
-        </div>
+<div class="login-box">
+    <div class="login-logo">
+      <a href=""><b>Staff</b>Login</a>
     </div>
- </div>
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+  
+        <form action="" method="post">
+            @csrf
+          <div class="input-group mb-3">
+            <input type="email" name="email" class="form-control" placeholder="Email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" name="password" class="form-control" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
+                <input type="checkbox" id="remember">
+                <label for="remember">
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+  
+        <div class="social-auth-links text-center mb-3">
+          <p>- OR -</p>
+          <a href="#" class="btn btn-block btn-primary">
+            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+          </a>
+          <a href="#" class="btn btn-block btn-danger">
+            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+          </a>
+        </div>
+        <!-- /.social-auth-links -->
+  
+        <p class="mb-1">
+          <a href="forgot-password.html">I forgot my password</a>
+        </p>
+        <p class="mb-0">
+          <a href="register.html" class="text-center">Register a new membership</a>
+        </p>
+      </div>
+      <!-- /.login-card-body -->
+    </div>
+  </div>
 
 
-</div>
-@endsection
+<!-- jQuery -->
+<script src="{{ asset("plugins/jquery/jquery.min.js") }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset("plugins/jquery-ui/jquery-ui.min.js") }}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<!-- ChartJS -->
+<script src="{{ asset("plugins/chart.js/Chart.min.js") }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset("plugins/sparklines/sparkline.js") }}"></script>
+<!-- JQVMap -->
+<script src="{{ asset("plugins/jqvmap/jquery.vmap.min.js") }}"></script>
+<script src="{{ asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset("plugins/jquery-knob/jquery.knob.min.js") }}"></script>
+<!-- daterangepicker -->
+<script src="{{ asset("plugins/moment/moment.min.js") }}"></script>
+<script src="{{ asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
+<!-- Summernote -->
+<script src="{{ asset("plugins/summernote/summernote-bs4.min.js") }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset("dist/js/adminlte.js") }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset("dist/js/demo.js") }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset("dist/js/pages/dashboard.js")}}"></script>
+</body>
+</html>
