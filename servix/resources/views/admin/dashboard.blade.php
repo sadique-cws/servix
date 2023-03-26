e4a5f9f60deef4c04581197154949f94bc9a3130
md-wasik-alam <wasikalam02@gmail.com>
2023-03-26 13:40:22 +0530

    dashboard isue fixed

--- servix/resources/views/admin/dashboard.blade.php
+++ servix/resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+  
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
@@ -605,5 +605,5 @@
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-  </div>
+
 @endsection
