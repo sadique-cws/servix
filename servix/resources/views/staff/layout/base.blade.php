5e3f33377a1277ec443067c0bbfdf86a90e6eeab
aditya-shekhar773 <adityashekhar.773@gmail.com>
2023-04-11 11:47:01 +0530

    staff search done

--- servix/resources/views/staff/layout/base.blade.php
+++ servix/resources/views/staff/layout/base.blade.php
@@ -28,6 +28,7 @@
     <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
