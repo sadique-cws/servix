52f58754631dd3424f88986cfa86e3424b5eb64d
LazyDeveloperr <chhotaslaman07@gmail.com>
2023-04-17 11:59:26 +0530

    contact learn flash done

---
+++ servix/resources/views/contact.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.layout')
+
+@section('contents')
+
+    <div class="container px-5" style="margin-top: 200px">
+        <div class="row justify-content-center px-auto">
+            <div class="col-lg-6 col-md-9 col-12">
+                <h1 class="look_ing my-0 pl-5">We'd love to hear <br> from you</h1>
+                <h4 class="train_ing pt-2 pl-5">You have ant question, comments or feedback please don't hesitate to get
+                    in touch with us.</h4>
+            </div>
+        </div>
+        <div class="container" style="margin-top: 150px;">
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 col-12 mb-3">
+                            <div class="card card_contact px-4 py-3">
+                                <h3 class="my-0"><b>Services</b></h3>
+                                <h4 class="my-0">Query</h4>
+                                <div class="d-flex pt-5">
+                                    <img src="Assets/Whatsapp.svg" class="img_whats" alt="logo">
+                                    <h5 class="pt-2 pl-2"><b>+91 7480897732</b></h5>
+                                </div>
+                                <div class="d-flex">
+                                    <img src="Assets/call.svg" class="img_whats" alt="logo">
+                                    <h5 class="pt-2 pl-2"><b>+91 9523327578</b></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-12 mb-3">
+                            <div class="card card_contact1 p-3">
+                                <h3 class="my-0"><b>Training</b></h3>
+                                <h4 class="my-0">Query</h4>
+                                <div class="d-flex pt-5">
+                                    <img src="Assets/Whatsapp.svg" class="img_whats" alt="logo">
+                                    <h5 class="pt-2 pl-2"><b>+91 7480897732</b></h5>
+                                </div>
+                                <div class="d-flex">
+                                    <img src="Assets/call.svg" class="img_whats" alt="logo">
+                                    <h5 class="pt-2 pl-2"><b>+91 9523327578</b></h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card card_contact2 p-3 mt-4">
+                        <div class="text-center">
+                            <img src="Assets/map.svg" class="img_whats2" alt="logo">
+                            <h3 class="pt-3 zila_school">Near MI Care, Zila School Road</h3>
+                            <h3 class="zila_school">Purnia, Bihar 854301</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container my-5">
+            <div class="text-center py-5">
+                <h1 class="it_repair">We provide best service </h1>
+                <h1 class="it_repair">&amp; Training</h1>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- servix/resources/views/flashMessage.blade.php
+++ servix/resources/views/flashMessage.blade.php
@@ -11,19 +11,27 @@
                         <h1 class="font-bold text-3xl text-gray-900 mb-4">Request submited successfully!</h1>
                     </div>
                     <div class="text-center">
-                      <p>Dear <b class="">{{$owner_name}}</b> Your request has been submited successfully!<br>Wait for our staffs to review your request. We will try our best to start work on your requested issue as soon as possible... 
+                      <p>Your request has been submited successfully!<br>Wait for our staffs to review your request. We will try our best to start work on your requested issue as soon as possible... 
                     </p>
                     <b><span class="fs-3" style="margin-right: 90px">Here is your service code </span></b><br>
-                    <div class="w-full d-flex flex-lg-row flex-sm-col ">
+                    <div class="w-full d-flex flex-lg-row flex-sm-col">
                         <div class="box1 col-8 justify-content-end align-items-center d-flex" style="height: 100px;">
-                            <code class="font-style-mono fs-1 text-success border px-5 py-2 overflow-auto" style="max-width: 450px; max-height: 80px; margin-inline: -30px">{{$service_code}}</code>
+                            <code id="copy-text" class="font-style-mono fs-1 text-success border px-5 py-2 overflow-auto text-uppercase " style="max-width: 450px; max-height: 80px; background-color: #D3D3D3" >iiiryfho</code>
+                            <div class="flex-row border font-style-mono p-1" style="margin-right: -15px" >
+                                <div class="" id="copy-button">
+                                    <svg id="copy-svg" style="height:48px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.4" d="M8 12.2002H15" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="0.4" d="M8 16.2002H12.38" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M10 6H14C16 6 16 5 16 4C16 2 15 2 14 2H10C9 2 8 2 8 4C8 6 9 6 10 6Z" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 4.02002C19.33 4.20002 21 5.43002 21 10V16C21 20 20 22 15 22H9C4 22 3 20 3 16V10C3 5.44002 4.67 4.20002 8 4.02002" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                </div>
+                                <div class="flex-row" style="font-size: 13px;" id="copy-message" >
+                                   Copy Code
+                                </div>
+                            </div>
                         </div>
                         <div class="box2 col-4 justify-content-start align-items-center d-flex overflow-visible " style="height: 100px;">
                             <div class="" style="height:220px; margin-top:-80px; margin-left:-10px; " id="animation-copy"></div>
                         </div>
                     </div>
+                    <p id="copy-message"></p>
                     <p>Please note it down anywhere! It may be asked while recieving your pruduct.. </p>
-                    </div>
                 </div>
             </div>
         </div>
@@ -49,6 +57,25 @@
         autoplay: true,
         path: '{{ asset('img/copyworker.json') }}'
     });
+     
+    // 
+    const copyText = document.querySelector('#copy-text');
+    const copyButton = document.querySelector('#copy-button');
+    const copyMessage = document.querySelector('#copy-message');
+    const copyIcon = document.querySelector('#copy-svg')
+
+    copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(copyText.textContent)
+        .then(() => {
+        copyMessage.textContent = 'Code copied!';
+        copyMessage.classList.add('text-success');
+        })
+        .catch(() => {
+        copyMessage.textContent = 'Oops, something went wrong.';
+        });
+    });
+
+
 </script>
 
 @endsection
