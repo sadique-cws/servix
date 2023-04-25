c9ece207327acd8965bbb35e0feab3286f41813a
aditya-shekhar773 <adityashekhar.773@gmail.com>
2023-04-25 19:26:49 +0530

    status working

--- servix/resources/views/userDashboard/trackRequest.blade.php
+++ servix/resources/views/userDashboard/trackRequest.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.layout')
 
 @section('contents')
-    <div class="d-flex  fixed" style="margin-top: 27%">
+    <div class="" style="margin-top: 10%">
         <!-- Button to Open the Modal -->
 
-        <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="text-center p-10"  >
 
-            <form action="{{ route('track.status') }}" method="POST">
+            <form action="{{ route('track.status') }}" method="POST" class="text-center p-10" style="width:40%; height:100%; margin-left:30%;">
                 @csrf
-                <input type="search" name="search" placeholder="Enter status code" class="form-control" value={{($searchStatus)?"$searchStatus":""}}  >
-                <button type="submit" class="btn btn-primary d-flex mx-auto btn-lg mt-2"> Track your order</button>
+                    <h3 class="text-uppercase font-weight-bold">Enter Your Servic Code</h3>
+                    <input type="search" name="search" placeholder="Enter status code" class="form-control text-center" style="margin-top: 5%" value={{($searchStatus)?"$searchStatus":""}}  >
+                    <button type="submit" class="btn btn-primary d-flex mx-auto btn-lg mt-2"> Track your order</button>
+                    @error('search')
+                        <p class="text-error">{{ $message }}</p>
+                    @enderror
+               
             </form>
 
             @if ($item!='')  
                            
-                <div class="card mb-3 mt-2" style="max-width: 540px;">
+                <div class="card mb-3 mt-2" style="max-width: 540px; margin-left:30%">
                     <div class="row no-gutters">
                         <div class="col-md-4">
                             <img src="https://picsum.photos/200/300" class="card-img" alt="...">
