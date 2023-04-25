f14ee4588d07e03a8cf91bbcbfe1ed00c79cafc5
aditya-shekhar773 <adityashekhar.773@gmail.com>
2023-04-25 15:36:51 +0530

    insert add

--- servix/resources/views/receptioner/layouts/base.blade.php
+++ servix/resources/views/receptioner/layouts/base.blade.php
@@ -29,6 +29,7 @@
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
