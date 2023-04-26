5bf20a0e3ca55070710f822a872e89a48b177093
aditya-shekhar773 <adityashekhar.773@gmail.com>
2023-04-26 17:39:07 +0530

    web cam work

--- servix/resources/views/receptioner/requestForm.blade.php
+++ servix/resources/views/receptioner/requestForm.blade.php
@@ -134,5 +134,5 @@
         } );
     }
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
