fa258eaca9e41ccd2012262ba14a66238515aaea
LazyDeveloperr <chhotaslaman07@gmail.com>
2023-05-02 13:06:06 +0530

    Added `message view page in admin panel`

--- servix/resources/views/admin/messages.blade.php
+++ servix/resources/views/admin/messages.blade.php
@@ -38,21 +38,39 @@
     
                             <a data-toggle="modal"  href="{{route('admin.messagesRead.req',$item->id)}}" data-target="#view{{ $item->id }}" role="button"
                                 class=" btn btn-info"><i class="fas fa-eye"></i> View</a>
-                            <div class="modal fade " id="view{{ $item->id }}" tabindex="-1" role="dialog"
-                                aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable "
-                                    role="document">
+
+                                {{-- Message View Page --}}
+
+                            <div class="modal fade " id="view{{ $item->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable " role="document">
                                     <div class="modal-content ">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLongTitle">All new
-                                                Request</h5>
-                                            <button type="button" class="close" data-dismiss="modal"
-                                                aria-label="Close">
+                                        <div class="modal-header flex-column">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
+                                            <h5 class="modal-title" id="exampleModalLongTitle"> <span class="font-weight-bold">From:</span> <span class="">{{$item->first_name}}</span></h5>
+                                            @if($item->company)
+                                            <div class="">
+                                                 <span>{{$item->company}}</span>
+                                             </div>
+                                            @endif
                                         </div>
                                         <div class="modal-body">
-                                            <div class="flex-row col-12">
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            {{-- <div class="flex-row col-12">
                                                 <table class="table">
                                                     <tr>
                                                         <th>Service Code</th>
@@ -96,7 +114,7 @@
                                                    
                                                    
                                                 </table>
-                                            </div>
+                                            </div> --}}
                                         </div>
     
                                     </div>
@@ -106,8 +124,104 @@
                         </td>
                     </tr>
                 @endforeach
+                
             </tbody>
         </table>
+    </div>
+</div>
+<div class="">
+    <div class="modal-content ">
+        <div class="modal-header flex-column">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <h5 class="modal-title" id="exampleModalLongTitle"> <span class="font-weight-bold">From:</span> <span class="">{{$item->first_name}}</span></h5>
+            @if($item->company)
+            <div style="margin-top: -10px ">
+                 <span class="text-muted"> {{$item->company}}</span>
+             </div>
+            @endif
+        </div>
+        <div class="modal-body">
+            <div class="">
+                <span  class="font-weight-bold">Subject:</span> <span>{{$item->subject}}</span>  
+            </div>
+            <div class="">
+                <div class="border p-4 " style="border-radius: 20px;background-color: #ffe6df">
+                    <p class="font-weight-bold">Query:</p>
+                    <div class="px-5 py-0 text-lg fs-1">{{$item->message}}
+                    </div>
+                    <div class="px-5 py-0 text-md mt-3"><span class="font-weight-bold">Heared form</span> : {{$item->inspired_from}}</div>
+                </div>
+               <div class="px-4 py-3">
+                <div class="text-lg">user details:</div>
+                <div class="">- Email: <span>{{$item->email}}</span></div>
+                <div class="">- Phone: <span>{{$item->contact}}</span></div>
+                <div class=""></div>
+               </div>
+            </div>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            {{-- <div class="flex-row col-12">
+                <table class="table">
+                    <tr>
+                        <th>Service Code</th>
+                        <td class="text-uppercase">{{ $item->id }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>First name</th>
+                        <td>{{ $item->first_name }}</td>
+                    </tr>
+                    <tr>
+                        <th>Last Name</th>
+                        <td>{{ $item->last_name }}</td>
+                    </tr>
+                    <tr>
+                        <th>Contact</th>
+                        <td>{{ $item->contact }}</td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td>{{ $item->email }}</td>
+                    </tr>
+                    <tr>
+                        <th>company</th>
+                        <td>{{ $item->company ? "$item->company" : 'N/A' }}</td>
+                    </tr>
+                    <tr>
+                        <th>Subject</th>
+                        <td>{{$item->subject}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th> Message</th>
+                        <td>{{ $item->message  }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th> inspired_from</th>
+                        <td>{{ $item->inspired_from  }}</td>
+                    </tr>
+                   
+                   
+                </table>
+            </div> --}}
+        </div>
+
     </div>
 </div>
 
---
+++ servix/resources/views/admin/messagesView.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout.base')
+
+
+@section('content')
+    <div class="">
+        
+    </div>
+@endsection
